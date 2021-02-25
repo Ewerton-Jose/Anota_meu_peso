f4545aef5fce21dbcbb632f3fe0faf2f64fd6c70
Ewerton j. Silva <76182422+Ewerton-Jose@users.noreply.github.com>
2021-02-24 21:39:07 -0300

    r

--- AnotaMeuPeso.py
+++ AnotaMeuPeso.py
@@ -1,9 +1,3 @@
-import time
-import datetime
-
-def Mostra_Data(x = []):
-    return f'{x[0]}/{x[1]}/{x[2]}'
-
 def Análise_float(x = str):
     while True:
         try:
@@ -32,33 +26,21 @@ def Análise_nome(x = 'Digite seu nome: '):
             return nome
             break
 
-def IMC(Peso = float, Altura = float):
-    x = []
-    imc = Peso / (Altura * Altura)
-    Classificação_de_peso = ''
-    
-    if imc <= 18.5:
-        Classificação_de_peso = 'Abaixo do peso'
-    elif imc >= 18.6 and imc <= 24.9:
-        Classificação_de_peso = 'Normal'
-    elif imc >= 24.9 and imc <= 29:
-        Classificação_de_peso = 'Sobre Peso'
-    elif imc >= 30 and imc <= 34.9:
-        Classificação_de_peso = 'Obesidade Primeiro Grau'
-    elif imc >= 35 and imc <= 39.9:
-        Classificação_de_peso = 'Obesidade Segundo Grau'
-    elif imc >= 40:
-        Classificação_de_peso = 'Obesidade mobirda'
-    
-    x.append(imc)
-    x.append(Classificação_de_peso)
-    return x
+Peso = Análise_float('Digite seu peso: ')
+Altura = Análise_float('Digite sua altura: ')
 
-data_Primeiro_Peso = f'{datetime.date.today()}'.split('-')
-# nome = Análise_nome('Digite seu nome: ')
-primeiro_Peso = Análise_float('Quanto você está pesando hoje: ')
-Altura_atual = Análise_float('Sua altura: ')
-lista = [IMC(primeiro_Peso, Altura_atual)]
-print(lista)
+imc = Peso / (Altura * Altura)
 
-# print(f'\033[32mNa data de {Mostra_Data(data_Primeiro_Peso)} você está pesando {primeiro_Peso} \nParabens!! para seu 1º dia até que você tá bem, continue assim {nome}\033[m')
+if imc <= 18.5:
+    Classificação_de_peso = 'Abaixo do peso'
+    dica = 
+elif imc >= 18.6 and imc <= 24.9:
+    Classificação_de_peso = 'Normal'
+elif imc >= 24.9 and imc <= 29:
+    Classificação_de_peso = 'Sobre Peso'  
+elif imc >= 30 and imc <= 34.9:
+    Classificação_de_peso = 'Obesidade Primeiro Grau'  
+elif imc >= 35 and imc <= 39.9:  
+    Classificação_de_peso = 'Obesidade Segundo Grau'
+elif imc >= 40:
+    Classificação_de_peso = 'Obesidade mobirda'
